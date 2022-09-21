584ba1f95a4bb565207747546c916ccb057a05f3
edoka <peter.edoka.242464@unn.edu.ng>
2022-09-20 19:45:28 -0700

    hotfix

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number < 0:
-        ld = 0 - (abs(number) % 10)
-    else:
-        ld = number % 10
-    return ld
+        return - (abs(number) % 10)
+    elif number == 0:
+        return '00'
+    return number % 10
