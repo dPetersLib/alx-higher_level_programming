f0f9698cabf69c68b7bcfefcfd38cfaa9deab980
edoka <peter.edoka.242464@unn.edu.ng>
2022-09-20 18:52:24 -0700

    manual str.upper()

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def uppercase(str):
-    if ord(str) in range(65, 91):
-        return True
-    return False
+    for i in str:
+        print(chr(ord(i) - 32), end='')
+    print('')
