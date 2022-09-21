e01c3c423bccf10ff046f41c33029613128d325c
edoka <peter.edoka.242464@unn.edu.ng>
2022-09-20 18:32:46 -0700

    chmod +x and pep8 hotfix

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,5 @@ for i in range(8):
     for j in range(10):
         if i > j or i == j:
             continue
-        print("{}{}".format(i,j), end=', ')
+        print("{}{}".format(i, j), end=', ')
 print(89)
