e2401c249a7572277531bdb7505283f2491256e6
edoka <peter.edoka.242464@unn.edu.ng>
2022-09-20 20:26:35 -0700

    tech interview prep - init

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * insert_node - function to insert a node
+ * @head: insertion location
+ * @number: node to bbe inserted
+ *
+ * Description: function to insert a node
+ *
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	//
+	//
+	return NULL;
+}
