8b87c522e1ff95d80b870e2f10b9b1226986eeb5
edoka <peter.edoka.242464@unn.edu.ng>
2022-09-20 19:05:06 -0700

    hotfix

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in str:
-        print("{}".format(chr(ord(i) - 32)), end='')
+        if ord(i) in range(65, 91):
+           order = ord(i)
+        else:
+            order = ord(i) - 32
+        print("{}".format(chr(order), end='')
     print('')
