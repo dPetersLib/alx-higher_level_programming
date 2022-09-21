9b38fd56c054873f9a45495c706ab9d8a3263160
edoka <peter.edoka.242464@unn.edu.ng>
2022-09-20 19:27:48 -0700

    special chars inclusion

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in str:
-        if ord(i) in range(65, 91):
+        if ord(i) in range(32, 91):
             order = ord(i)
         else:
             order = ord(i) - 32
