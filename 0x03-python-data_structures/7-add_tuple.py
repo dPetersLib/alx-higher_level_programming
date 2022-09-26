d8de68cc1581cf1678c3dea00bc13bf2aafa0077
edoka <peter.edoka.242464@unn.edu.ng>
2022-09-25 17:37:22 -0700

    tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -6,6 +6,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
             res.append(tuple_a[i] + 0)
         elif i > len(tuple_a) - 1:
             res.append(0 + tuple_b[i])
+        elif i > len(tuple_a) - 1 and i > len(tuple_b) - 1:
+            res.append(0)
         else:
             res.append(tuple_a[i] + tuple_b[i])
     return tuple(res)
