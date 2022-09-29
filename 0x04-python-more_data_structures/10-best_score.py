46e3ae69c5a78b4546e06941f779dc992d87be01
edoka <peter.edoka.242464@unn.edu.ng>
2022-09-29 04:23:40 +0100

    best score hotfix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        max = 0
-        for i in a_dictionary:
-            if a_dictionary[i] > max:
-                max = a_dictionary[i]
+        max = ""
+        max_score = 0
+        for key, value in a_dictionary.items():
+            if value > max_score:
+                max = key
+                max_score = value
         return max
     return None
