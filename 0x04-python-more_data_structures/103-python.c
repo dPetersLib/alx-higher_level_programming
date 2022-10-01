d4e752c0d47ee5da4ed4df587e0a61d2f7074235
edoka <peter.edoka.242464@unn.edu.ng>
2022-10-01 02:53:23 +0100

    cpython

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,23 @@
+#include <Python.h>
+
+/**
+ * vprint_python_list - prints basic info about python list
+ * @p: A PyObject list.
+ * 
+ */
+
+void print_python_list(PyObject *p)
+{
+
+}
+
+/**
+ * print_python_bytes - prints basic info about python bytes objects
+ * @p: A PyByteObject
+ * 
+ */
+
+void print_python_bytes(PyObject *p)
+{
+    
+}
