e463c8e3e4c9664667b8d3e7e8a7ad987452ce42
edoka <peter.edoka.242464@unn.edu.ng>
2022-10-11 17:19:00 +0100

    My first square + docstrings

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""OOP implementation of Square
+
+This module models a square. The square is empty for now.
+"""
 
 
 class Square:
+    """A class that defines a square
+
+    An empty class Square that defines a square.
+    the <pass> keyword is used to signify that the class is empty.
+    """
     pass
