310b0b1701c08d2d0e84b744ee13a996c78685e7
edoka <peter.edoka.242464@unn.edu.ng>
2022-10-12 16:23:50 +0100

    ByteCode -> Python #5

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Magic Class Module
+This module contains our magic class"""
+
+
+class MagicClass:
+    """MagicClass class"""
+    def __init__(self, radius):
+        """This is the constructor of the class"""
+        self._MagicClass__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError('radius must be a number')
+        self._MagicClass__radius = radius
+
+    def area(self):
+        """this calculates and returns the area"""
+        return self._MagicClass__radius ** 2 * math.pi
+
+    def circumference(self):
+        """this calculates and returns the circumference"""
+        return 2 * math.pi * self._MagicClass__radius
+
+from dis import dis
+dis(MagicClass)
