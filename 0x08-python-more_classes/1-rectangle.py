2dc9a897bd84e71479766c78f020770cc872442c
edoka <peter.edoka.242464@unn.edu.ng>
2022-10-17 18:26:34 +0100

    Real definition of a rectangle: docstrings update

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,34 +1,33 @@
 #!/usr/bin/python3
-"""
-1-rectangle module
-==================
+"""1-rectangle module.
 
-Based on the first `0-rectangle`
+Based on the first `0-rectangle`.
 
 """
 
 
 class Rectangle:
-    """Real Rectangle
+    """Real Rectangle.
 
-    Defines a real rectangle
+    Defines a real rectangle.
     """
+
     def __init__(self, width=0, height=0):
-        """Instantiation method
+        """Instantiate a rectangle.
 
         This constructor method is call just once,
         i.e. when an instance of this class is initiated.
 
         Args:
-            width (int): width of the rectangle instance
-            height (int): height of the rectangle
+            width (int): width of the rectangle instance.
+            height (int): height of the rectangle.
         """
         self.__height = height
         self.__width = width
 
     @property
     def width(self):
-        """Width property of a Rectangle"""
+        """Width property of a Rectangle."""
         return self.__width
 
     @width.setter
@@ -41,7 +40,7 @@ class Rectangle:
 
     @property
     def height(self):
-        """Height property of a Rectangle"""
+        """Height property of a Rectangle."""
         return self.__height
 
     @height.setter
