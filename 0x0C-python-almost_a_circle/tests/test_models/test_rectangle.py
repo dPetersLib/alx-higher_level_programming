d18d3349fc71be71b6c72b000eb0ea770cfa0189
edoka <peter.edoka.242464@unn.edu.ng>
2023-01-04 12:01:15 +0100

    more tests on rectangle

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -3,6 +3,9 @@ from models.rectangle import Rectangle
 
 
 class TestRectangleMethods(unittest.TestCase):
+
+    # Exceptions Test
+
     def test_width_is_int(self):
         with self.assertRaises(TypeError):
             rec = Rectangle('5', 50)
@@ -10,3 +13,33 @@ class TestRectangleMethods(unittest.TestCase):
     def test_correct_width(self):
         with self.assertRaises(ValueError):
             rec = Rectangle(-20, 40)
+
+    def test_height_is_int(self):
+        with self.assertRaises(TypeError):
+            rec = Rectangle(5, '2')
+
+    def test_correct_height(self):
+        with self.assertRaises(ValueError):
+            rec = Rectangle(20, -5)
+
+    def test_x_is_int(self):
+        with self.assertRaises(TypeError):
+            rec = Rectangle(5, 50, '4')
+
+    def test_correct_x(self):
+        with self.assertRaises(ValueError):
+            rec = Rectangle(20, 40, -3)
+
+    def test_y_is_int(self):
+        with self.assertRaises(TypeError):
+            rec = Rectangle(5, 50, 4, '23')
+
+    def test_correct_y(self):
+        with self.assertRaises(ValueError):
+            rec = Rectangle(20, 40, 3, -23)
+
+    # Area Tests
+
+    def test_area(self):
+        rec = Rectangle(2, 4)
+        self.assertEqual(rec.area(), 8)
